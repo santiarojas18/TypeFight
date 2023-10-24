db549546ae4b6f51fd93102c8d89437a7b7f10fb
AndresOnate <andres.onate@mail.escuelaing.edu.co>
2023-10-24 13:26:52 -0500

    Front para ingresar palabra

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,50 +1,10 @@
 var app = (function () {
 
-    class Point{
-        constructor(x,y){
-            this.x=x;
-            this.y=y;
-        }        
-    }
-
-    var tableId = null;
     var stompClient = null;
     var listenersAdded = false;
+    const userWord = document.getElementById("userWord");
 
-    var addPointToCanvas = function (point) {        
-        var canvas = document.getElementById("canvas");
-        var ctx = canvas.getContext("2d");
-        ctx.beginPath();
-        ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
-        ctx.stroke();
-    };
 
-    var paintPolygon = function (points) {
-        var currentPoint;
-        var nextPoint;
-        var canvas = document.getElementById("canvas");
-        var ctx = canvas.getContext("2d");
-        for (var i = 0; i < points.length - 1; i++) {
-            currentPoint = points[i];
-            addPointToCanvas(currentPoint);
-            nextPoint = points[i + 1];
-            ctx.beginPath();
-            ctx.moveTo(currentPoint.x, currentPoint.y);
-            ctx.lineTo(nextPoint.x ,nextPoint.y);
-            ctx.stroke();
-            ctx.closePath();
-        }
-        currentPoint = points[points.length - 1];
-        addPointToCanvas(currentPoint);
-        nextPoint = points[0];
-        ctx.beginPath();
-        ctx.moveTo(currentPoint.x, currentPoint.y);
-        ctx.lineTo(nextPoint.x ,nextPoint.y);
-        ctx.stroke();
-        ctx.closePath();
-    };
-    
-    
     var getMousePosition = function (evt) {
         canvas = document.getElementById("canvas");
         var rect = canvas.getBoundingClientRect();
@@ -63,18 +23,11 @@ var app = (function () {
         };
     };
 
-    var cleanCanvas = function () {
-        var canvas = document.getElementById("canvas");
-        var ctx = canvas.getContext("2d");
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    };
-
     var connectAndSubscribe = function () {
         console.info('Connecting to WS...');
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
 
-        //subscribe to /topic/newpoint.tableId when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
             stompClient.subscribe('/topic/newpoint.' + tableId, function (eventbody) {
@@ -92,23 +45,21 @@ var app = (function () {
     };
     
     
-
     return {
-
         addListeners: function () {
-            var can = document.getElementById("canvas");
-            var positions;
-            if(window.PointerEvent) {
-                canvas.addEventListener("pointerdown", function(event){
-                    positions = getMousePositionWithPage(event);
-                    app.publishPoint(positions.x, positions.y);
-                });
-            } else {
-                canvas.addEventListener("mousedown", function(event){
-                    positions = getMousePosition(event);
-                    app.publishPoint(positions.x, positions.y);
-                });
-            }
+            document.addEventListener("keydown", function(event) {
+                if (event.key.length === 1) {
+                    // Solo se procesa la entrada si es una letra (no se procesan teclas especiales, números, etc.).
+                    const currentWord = userWord.textContent;
+                    const newWord = currentWord + event.key;
+                    userWord.textContent = newWord;
+                } else if (event.key === "Backspace") {
+                    // Manejar la tecla de retroceso (Backspace) para borrar la última letra.
+                    const currentWord = userWord.textContent;
+                    const newWord = currentWord.slice(0, -1); // Elimina el último carácter.
+                    userWord.textContent = newWord;
+                }
+            });            
         },
 
         init: function (newTableId) {
@@ -117,8 +68,6 @@ var app = (function () {
                 listenersAdded = true;
             }
 
-            tableId = newTableId;
-            cleanCanvas();
 
             //disconnect connection
             app.disconnect();
